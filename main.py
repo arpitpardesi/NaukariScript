596d696c09c1f8af61a360c8d05a68e97b5f8af7
Arpit Pardesi <arpit.pardesi6@gmail.com>
2024-12-20 00:25:44 +0530

    webdriver loaded

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+import json
+import requests
+import os
+from datetime import datetime, timedelta
 
-driver = webdriver.Chrome()
+
+options = Options()
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+driver.get('http://www.naukri.com')
+time.sleep(3)
+# driver.quit()
