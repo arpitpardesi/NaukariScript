4df08f103f0d7b56f73c10e3999538c68f10fa24
Arpit Pardesi <arpit.pardesi6@gmail.com>
2024-12-20 23:46:32 +0530

    auto id pass feature added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -13,15 +14,37 @@ from datetime import datetime, timedelta
 import config
 from config import username
 
-userName = config.username
-password = config.password
+def login():
+    pass
+
+user = config.username
+pwd = config.password
+url = "https://www.naukri.com/nlogin/login"
 
 options = Options()
 driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-driver.get('http://www.naukri.com')
+driver.get(url)
+
 login = driver.find_element(By.ID, 'login_Layer')
 if login:
-
     login.click()
 time.sleep(3)
+
+username = driver.find_element(By.ID, 'usernameField')
+if username:
+    username.clear()
+    username.send_keys(user)
+
+password = driver.find_element(By.ID, 'passwordField')
+if password:
+    password.clear()
+    password.send_keys(pwd)
+    password.send_keys(Keys.RETURN)
+time.sleep(3)
+
+# submit = driver.find_element(By.ID, 'g-recaptcha')
+# # submit = driver.find_element(By.ID, 'btn-login')
+# if submit:
+#     submit.click()
+
 # driver.quit()
